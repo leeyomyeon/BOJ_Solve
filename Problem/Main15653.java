fef7ef92580512003c8c0ead9622b7ccad6a9161
leeyomyeon <yyiz1135@gmail.com>
2022-12-06 14:49:06 +0900

    15653 - 구슬 탈출 4

--- Problem/Main15653.java
+++ Problem/Main15653.java
@@ -1,16 +1,114 @@
 import java.io.*;
+import java.util.ArrayDeque;
 import java.util.StringTokenizer;
 
 public class Main15653 {
     public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out), 1024 * 64);
+    public static int R, C, res;
+    public static char[][] arr;
+    public static boolean[][][][] visited;
+    public static ArrayDeque<Point> deque;
+    public static class Point {
+        int redR, redC, blueR, blueC, dir, cnt;
 
+        public void setRedR(int redR) {
+            this.redR = redR;
+        }
+        public void setRedC(int redC) {
+            this.redC = redC;
+        }
+        public void setBlueR(int blueR) {
+            this.blueR = blueR;
+        }
+        public void setBlueC(int blueC) {
+            this.blueC = blueC;
+        }
+
+        public Point(int redR, int redC, int blueR, int blueC, int dir, int cnt) {
+            this.redR = redR;
+            this.redC = redC;
+            this.blueR = blueR;
+            this.blueC = blueC;
+            this.dir = dir;
+            this.cnt = cnt;
+        }
+    }
     public static void main(String[] args) throws Exception {
         FastReader fr = new FastReader();
-        bw.write("");
+        R = fr.nextInt();
+        C = fr.nextInt();
+        Point start = new Point(0, 0, 0, 0, -1, 0);
+        arr = new char[R][C];
+        for(int i = 0; i < R; i++) {
+            for(int j = 0; j < C; j++) {
+                arr[i][j] = fr.nextChar();
+                if(arr[i][j] == 'R') {
+                    arr[i][j] = '.';
+                    start.setRedR(i);
+                    start.setRedC(j);
+                } else if(arr[i][j] == 'B') {
+                    arr[i][j] = '.';
+                    start.setBlueR(i);
+                    start.setBlueC(j);
+                }
+            }
+        }
+        res = move(start);
+        bw.write(Integer.toString(res));
         bw.flush();
         bw.close();
     }
+    public static int move(Point start) {
+        // 왔던 방향, 반대방향은 갈 필요 없음
+        visited = new boolean[R][C][R][C];
+        visited[start.redR][start.redC][start.blueR][start.blueC] = true;
+        deque = new ArrayDeque<>();
+        deque.add(start);
+        while(!deque.isEmpty()) {
+            Point current = deque.removeFirst();
+            if(arr[current.redR][current.redC] == 'O') {
+                return current.cnt;
+            }
+            int[] next = current.dir == -1 ? new int[]{0, 1, 2, 3} : nextDir[current.dir % 2];
+            for(int d : next) {
+                int nRedR = current.redR;
+                int nRedC = current.redC;
+                int nBlueR = current.blueR;
+                int nBlueC = current.blueC;
 
+                while(arr[nRedR + dr[d]][nRedC + dc[d]] != '#' && arr[nRedR][nRedC] != 'O') {
+                    nRedR += dr[d];
+                    nRedC += dc[d];
+                }
+                while(arr[nBlueR + dr[d]][nBlueC + dc[d]] != '#' && arr[nBlueR][nBlueC] != 'O') {
+                    nBlueR += dr[d];
+                    nBlueC += dc[d];
+                }
+                if(nRedR == nBlueR && nRedC == nBlueC) {
+                    if(arr[nRedR][nRedC] == 'O') {
+                        continue;
+                    }
+                    if(Math.abs(nRedR - current.redR) + Math.abs(nRedC - current.redC) > Math.abs(nBlueR - current.blueR) + Math.abs(nBlueC - current.blueC)) {
+                        nRedR -= dr[d];
+                        nRedC -= dc[d];
+                    } else {
+                        nBlueR -= dr[d];
+                        nBlueC -= dc[d];
+                    }
+                }
+                if(!visited[nRedR][nRedC][nBlueR][nBlueC]) {
+                    visited[nRedR][nRedC][nBlueR][nBlueC] = true;
+                    deque.add(new Point(nRedR, nRedC, nBlueR, nBlueC, d, current.cnt + 1));
+                }
+            }
+        }
+
+        return -1;
+    }
+    // 상 우 하 좌
+    public static int[] dr = {-1, 0, 1, 0};
+    public static int[] dc = {0, 1, 0, -1};
+    public static int[][] nextDir = {{1, 3}, {0, 2}};
     public static class FastReader {
         private final DataInputStream din;
         private final int BUFFER_SIZE = 1 << 16;
@@ -109,6 +207,14 @@ public class Main15653 {
                 fillBuffer();
             }
             return buffer[bufferPointer++];
+        }
+        private char nextChar() throws IOException {
+            byte c = read();
+            if(c == '\n') {
+                c = read();
+            }
+
+            return (char) c;
         }
     }
 }
