aa24d27acf391fb6e8f68135349a8f083f50bac6
lee yo myeon <44407364+leeyomyeon@users.noreply.github.com>
2022-12-06 10:51:54 +0900

    11051 - 이항 계수 2

---
+++ Problem/Main11051.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+// 파스칼의 삼각형(BigInteger)
+public class Main11051 {
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws Exception {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+
+        BigInteger N = BigInteger.ONE;
+        BigInteger R = BigInteger.ONE;
+
+        for(int i = 0; i < r; i++) {
+            N = N.multiply(new BigInteger(String.valueOf(n - i)));
+            R = R.multiply(new BigInteger(String.valueOf(r - i)));
+        }
+        bw.write(N.divide(R).mod(new BigInteger("10007")).toString());
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
