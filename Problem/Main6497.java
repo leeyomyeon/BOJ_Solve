6042a2facbe33a5daeda92f7f6a56615109deab8
lee yo myeon <44407364+leeyomyeon@users.noreply.github.com>
2022-12-06 10:29:14 +0900

    6497 - 전력난

---
+++ Problem/Main6497.java
@@ -0,0 +1,141 @@
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main6497 {
+    public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out), 1024 * 64);
+    public static int N, M;
+    public static int[] arr;
+    public static ArrayList<Node> list;
+    public static class Node implements Comparable<Node>{
+        int from, to, weight;
+        public Node(int from, int to, int weight) {
+            this.from = from;
+            this.to = to;
+            this.weight = weight;
+        }
+        @Override
+        public int compareTo(Node o) {
+            return this.weight - o.weight;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        FastReader fr = new FastReader();
+        while(true) {
+            N = fr.nextInt();
+            M = fr.nextInt();
+            if(N == 0 && M == 0) {
+                break;
+            }
+            list = new ArrayList<>();
+            arr = new int[N];
+            for(int i = 0; i < N; i++) {
+                arr[i] = i;
+            }
+            for(int i = 0; i < M; i++) {
+                int from = fr.nextInt();
+                int to = fr.nextInt();
+                int weight = fr.nextInt();
+                list.add(new Node(from, to, weight));
+            }
+
+            Collections.sort(list);
+            int res = 0;
+
+            for(int i = 0; i < list.size(); i++) {
+                Node current = list.get(i);
+                int x = find(current.from);
+                int y = find(current.to);
+                if(x == y) {
+                    res += current.weight;
+                    continue;
+                }
+
+                union(current.from, current.to);
+            }
+            bw.write(Integer.toString(res));
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+    }
+    public static void union(int from, int to) {
+        int x = find(from);
+        int y = find(to);
+        if(x < y) {
+            arr[y] = x;
+        } else {
+            arr[x] = y;
+        }
+    }
+    public static int find(int target) {
+        if(target == arr[target]) {
+            return target;
+        }
+
+        return arr[target] = find(arr[target]);
+    }
+    public static class FastReader {
+        private final DataInputStream din;
+        private final int BUFFER_SIZE = 1 << 16;
+        private final byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        public FastReader() {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public String readLine() throws IOException {
+            byte[] buf = new byte[BUFFER_SIZE]; // input line length
+            int cnt = 0, c;
+            while ((c = read()) != -1) {
+                if (c == '\n') {
+                    break;
+                }
+                buf[cnt++] = (byte) c;
+            }
+            return new String(buf, 0, cnt);
+        }
+
+        public int nextInt() throws IOException {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ') {
+                c = read();
+            }
+            boolean neg = (c == '-');
+            if (neg) {
+                c = read();
+            }
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            if (neg) {
+                return -ret;
+            }
+            return ret;
+        }
+
+        private void fillBuffer() throws IOException {
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1) {
+                buffer[0] = -1;
+            }
+        }
+
+        private byte read() throws IOException {
+            if (bufferPointer == bytesRead) {
+                fillBuffer();
+            }
+            return buffer[bufferPointer++];
+        }
+
+    }
+}
