e07e247daad8df40f0f514c16dfdd6b6246ef072
lee yo myeon <44407364+leeyomyeon@users.noreply.github.com>
2022-12-06 09:09:15 +0900

    2167 - 2차원 배열의 합

---
+++ Problem/Main2167.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main2167 {
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in), 1024 * 64);
+    public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out), 1024 * 64);
+    public static int[][] dp;
+    public static int N, M;
+    public static void main(String[] args) throws Exception {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        dp = new int[N + 1][M + 1];
+        for(int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 1; j <= M; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + Integer.parseInt(st.nextToken());;
+            }
+        }
+        int K = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int k = 0; k < K; k++) {
+            st = new StringTokenizer(br.readLine());
+            int r1 = Integer.parseInt(st.nextToken());
+            int c1 = Integer.parseInt(st.nextToken());
+            int r2 = Integer.parseInt(st.nextToken());
+            int c2 = Integer.parseInt(st.nextToken());
+            int res = dp[r2][c2] - dp[r2][c1 - 1] - dp[r1 - 1][c2] + dp[r1 - 1][c1 - 1];
+            sb.append(res).append("\n");
+        }
+        bw.write(sb.toString());
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
