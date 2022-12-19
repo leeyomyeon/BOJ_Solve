7ded020cf3dd67063a7dbe857328a672f34e90d9
leeyomyeon <yyiz1135@gmail.com>
2022-12-19 17:21:23 +0900

    17408 - 수열과 쿼리 24

---
+++ Problem/Main17408.java
@@ -0,0 +1,135 @@
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+
+public class Main17408 {
+    public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out), 1024 * 64);
+    public static int N, M;
+    public static int[] arr;
+    public static int[][] segTree;
+    public static void main(String[] args) throws Exception {
+        FastReader fr = new FastReader();
+        N = fr.nextInt();
+        arr = new int[N + 1];
+        segTree = new int[N * 4][2];
+        for(int i = 1; i <= N; i++) {
+            arr[i] = fr.nextInt();
+            update(1, N, 1, i, arr[i]);
+        }
+        M = fr.nextInt();
+        for(int m = 0; m < M; m++) {
+            int command = fr.nextInt();
+            int a = fr.nextInt();
+            int b = fr.nextInt();
+            if(command == 1) {
+                update(1, N, 1, a, b);
+            } else {
+                int[] f = find(1, N, 1, a, b);
+                bw.write((f[0] + f[1]) + "\n");
+            }
+        }
+        bw.flush();
+        bw.close();
+    }
+    /*
+                    1
+                 2     3
+               4   5  6 7
+              8 9
+     */
+    public static void update(int start, int end, int idx, int target, int value) {
+        if(target < start || end < target) {
+            return;
+        }
+        if(start == end) {
+            segTree[idx][0] = value;
+            return;
+        }
+        int mid = (start + end) / 2;
+        update(start, mid, idx * 2, target, value);
+        update(mid + 1, end, idx * 2 + 1, target, value);
+        int[] sortedList = new int[]{segTree[idx * 2][0], segTree[idx * 2][1], segTree[idx * 2 + 1][0], segTree[idx * 2 + 1][1]};
+        Arrays.sort(sortedList);
+        segTree[idx][0] = sortedList[3];
+        segTree[idx][1] = sortedList[2];
+    }
+    public static int[] find(int start, int end, int idx, int left, int right) {
+        if(right < start || end < left) {
+            return new int[]{0, 0};
+        }
+        if(left <= start && end <= right) {
+            return segTree[idx];
+        }
+        int mid = (start + end) / 2;
+        int[] leftNode = find(start, mid, idx * 2, left, right);
+        int[] rightNode = find(mid + 1, end, idx * 2 + 1, left, right);
+        int[] sortedList = new int[]{leftNode[0], leftNode[1], rightNode[0], rightNode[1]};
+        Arrays.sort(sortedList);
+        int max = sortedList[3];
+        int nextMax = sortedList[2];
+        return new int[]{max, nextMax};
+    }
+    public static class FastReader {
+        private final DataInputStream din;
+        private final int BUFFER_SIZE = 1 << 16;
+        private final byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        public FastReader() {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public String readLine() throws IOException {
+            byte[] buf = new byte[BUFFER_SIZE]; // input line length
+            int cnt = 0, c;
+            while ((c = read()) != -1) {
+                if (c == '\n') {
+                    break;
+                }
+                buf[cnt++] = (byte) c;
+            }
+            return new String(buf, 0, cnt);
+        }
+
+        public int nextInt() throws IOException {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ') {
+                c = read();
+            }
+            boolean neg = (c == '-');
+            if (neg) {
+                c = read();
+            }
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            if (neg) {
+                return -ret;
+            }
+            return ret;
+        }
+
+        private void fillBuffer() throws IOException {
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1) {
+                buffer[0] = -1;
+            }
+        }
+
+        private byte read() throws IOException {
+            if (bufferPointer == bytesRead) {
+                fillBuffer();
+            }
+            return buffer[bufferPointer++];
+        }
+    }
+}
