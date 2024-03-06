a25117ec0b10ed4be7565c8ab9e079d0a6884adf
leeyomyeon <yyiz1135@gmail.com>
2024-03-06 16:14:36 +0900

    11286 - 절댓값 힙

--- Problem/Main11286.java
+++ Problem/Main11286.java
@@ -1,11 +1,7 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.util.PriorityQueue;
-// 클래스 사용
+
 public class Main11286 {
-    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in), 1024 * 64);
     public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out), 1024 * 64);
     public static PriorityQueue<Number> pq;
     public static int N;
@@ -19,17 +15,18 @@ public class Main11286 {
         @Override
         public int compareTo(Number o) {
             if(o.abs == this.abs) {
-                return this.origin - o.origin;
+                return Integer.compare(this.origin, o.origin);
             }
-            return this.abs - o.abs;
+            return Integer.compare(this.abs, o.abs);
         }
     }
     public static void main(String[] args) throws Exception {
-        N = Integer.parseInt(br.readLine());
+        FastReader fr = new FastReader();
+        N = fr.nextInt();
         pq = new PriorityQueue<>();
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i < N; i++) {
-            int num = Integer.parseInt(br.readLine());
+            int num = fr.nextInt();
             if(num == 0) {
                 if(pq.size() == 0) {
                     sb.append("0");
@@ -42,8 +39,77 @@ public class Main11286 {
             }
         }
         bw.write(sb.toString());
-        br.close();
         bw.flush();
         bw.close();
     }
+    public static class FastReader {
+        private final DataInputStream din;
+        private final int BUFFER_SIZE = 1 << 16;
+        private final byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        public FastReader() {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public String readLine() throws IOException {
+            byte[] buf = new byte[BUFFER_SIZE]; // input line length
+            int cnt = 0, c;
+            while ((c = read()) != -1) {
+                if (c == '\n') {
+                    break;
+                }
+                buf[cnt++] = (byte) c;
+            }
+            return new String(buf, 0, cnt);
+        }
+
+        public int nextInt() throws IOException {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ') {
+                c = read();
+            }
+            boolean neg = (c == '-');
+            if (neg) {
+                c = read();
+            }
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            if (neg) {
+                return -ret;
+            }
+            return ret;
+        }
+
+        private void fillBuffer() throws IOException {
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1) {
+                buffer[0] = -1;
+            }
+        }
+
+        private byte read() throws IOException {
+            if (bufferPointer == bytesRead) {
+                fillBuffer();
+            }
+            return buffer[bufferPointer++];
+        }
+
+    }
 }
+/*
+8
+-2147483647
+2147483646
+-2147483646
+2147483645
+0
+0
+0
+0
+ */
