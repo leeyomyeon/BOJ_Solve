632b786fd7671404558ac843ad85b22e6460839f
이요면 <yyiz1135@gmail.com>
2024-06-17 13:39:06 +0900

    1406 - 에디터

--- Problem/Main1406.java
+++ Problem/Main1406.java
@@ -1,53 +1,150 @@
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.InputStreamReader;
+import java.io.DataInputStream;
+import java.io.IOException;
 import java.io.OutputStreamWriter;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.util.ArrayDeque;
 
 public class Main1406 {
-    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in), 1024 * 64);
     public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out), 1024 * 64);
-    public static Stack<Character> leftStack, rightStack;
+    public static ArrayDeque<Character> left, right;
     public static void main(String[] args) throws Exception {
-        leftStack = new Stack<>();      // 커서 왼쪽
-        rightStack = new Stack<>();   // 커서 오른쪽
-        String str = br.readLine();
-        for(int i = 0; i < str.length(); i++) {
-            leftStack.add(str.charAt(i));
+        FastReader fr = new FastReader();
+        left = new ArrayDeque<>();
+        right = new ArrayDeque<>();
+        while(true) {
+            char c = (char) fr.read();
+            if(c <= ' ') {
+                break;
+            }
+            left.add(c);
         }
-        int N = Integer.parseInt(br.readLine());
-        for(int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            String command = st.nextToken();
-            if(command.equals("L")) {
-                if(!leftStack.isEmpty()) {
-                    rightStack.add(leftStack.pop());
-                }
-            } else if(command.equals("D")) {
-                if(!rightStack.isEmpty()) {
-                    leftStack.add(rightStack.pop());
-                }
-            } else if(command.equals("B")) {
-                if(!leftStack.isEmpty()) {
-                    leftStack.pop();
-                }
-            } else {
-                char target = st.nextToken().charAt(0);
-                leftStack.add(target);
+        int n = fr.nextInt();
+        for(int i = 0; i < n; i++) {
+            char c = fr.nextChar();
+            if(c == 'L' && !left.isEmpty()) {
+                right.addFirst(left.removeLast());
+            } else if(c == 'D' && !right.isEmpty()) {
+                left.addLast(right.removeFirst());
+            } else if(c == 'B' && !left.isEmpty()) {
+                left.removeLast();
+            } else if(c == 'P') {
+                char t = fr.nextChar();
+                left.addLast(t);
             }
         }
-        StringBuilder left = new StringBuilder();
-        StringBuilder right = new StringBuilder();
-        while(!leftStack.isEmpty()) {
-            left.append(leftStack.pop());
+        while(!left.isEmpty()) {
+            bw.write(left.removeFirst());
         }
-        while(!rightStack.isEmpty()) {
-            right.append(rightStack.pop());
+        while(!right.isEmpty()) {
+            bw.write(right.removeFirst());
         }
-        bw.write(left.reverse().append(right).toString());
-        br.close();
         bw.flush();
         bw.close();
+    }
+
+    public static class FastReader {
+        private final DataInputStream din;
+        private final int BUFFER_SIZE = 1 << 24;
+        private final byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        public FastReader() {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public String readLine() throws IOException {
+            byte[] buf = new byte[BUFFER_SIZE]; // input line length
+            int cnt = 0, c;
+            while ((c = read()) != -1) {
+                if (c == '\n') {
+                    break;
+                }
+                buf[cnt++] = (byte) c;
+            }
+            return new String(buf, 0, cnt);
+        }
+
+        public int nextInt() throws IOException {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ') {
+                c = read();
+            }
+            boolean neg = (c == '-');
+            if (neg) {
+                c = read();
+            }
+            do {
+                ret = (ret << 3) + (ret << 1) + (c & 15);
+            } while ((c = read()) > 32);
+
+            return neg ? ~ret + 1 : ret;
+        }
+
+        public long nextLong() throws IOException {
+            long ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = (ret << 3) + (ret << 1) + (c & 15);
+            }
+            while ((c = read()) >= '0' && c <= '9');
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public double nextDouble() throws IOException {
+            double ret = 0, div = 1;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+
+            do {
+                ret = ret * 10 + c - '0';
+            }
+            while ((c = read()) >= '0' && c <= '9');
+
+            if (c == '.') {
+                while ((c = read()) >= '0' && c <= '9') {
+                    ret += (c - '0') / (div *= 10);
+                }
+            }
+
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public char nextChar() throws IOException {
+            byte c = read();
+            while (c <= ' ') {
+                c = read();
+            }
+            return (char) c;
+        }
+
+        private void fillBuffer() throws IOException {
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1) {
+                buffer[0] = -1;
+            }
+        }
+
+        private byte read() throws IOException {
+            if (bufferPointer == bytesRead) {
+                fillBuffer();
+            }
+            return buffer[bufferPointer++];
+        }
     }
 }
